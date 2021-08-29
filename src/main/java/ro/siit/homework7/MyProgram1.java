1e36dce36f41c70efd85a3485514cf35ae91ab52
CosminJecan <85880477+CosminJecan@users.noreply.github.com>
2021-08-29 08:36:25 +0300

    tema session 19 - IO

---
+++ src/main/java/ro/siit/homework7/MyProgram1.java
@@ -0,0 +1,52 @@
+package ro.siit.homework7;
+
+
+public class MyProgram1 {
+//    private double value1;
+//    private double value2;
+//    private String operator;
+//
+//
+//    public MyProgram1() {
+//        this.value1 = value1;
+//        this.value2 = value2;
+//        this.operator = operator;
+//    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+
+//        String value1 = args[0];
+//        String value2 = args[1];
+//        String operator = args[2];
+//        MyProgram1 op = new MyProgram1();
+
+
+    }
+}
+//
+//
+//        switch ( operator ) {
+//            case "+":
+//                System.out.println(value1 + value2);
+//                break;
+//            case "-":
+//                System.out.println(value1 - value2);
+//                break;
+//            case "*":
+//                System.out.println(value1 * value2);
+//                break;
+//            case "/":
+//                System.out.println(value1 / value2);
+//                break;
+//            default:
+//                System.out.println("Invalid operation!");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(operator);
+//    }
+
